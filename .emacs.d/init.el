b3a4975400af24e8fd1b025b6fdd3f6ae1f5d894
jowj <me@jowj.net>
2018-03-14 15:14:57 -0500

    theme and default on open behavior changes.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,10 @@
 ;; -*- mode: elisp -*-
-
 ;; Disable the splash screen (to enable it agin, replace the t with 0)
 (setq inhibit-splash-screen t)
 
+
+;; open primary org file on launch
+(find-file "~/Dropbox/org/personal.org") 
 ;; Enable transient mark mode
 (transient-mark-mode 1)
 
@@ -14,26 +16,37 @@
 ;; The above is the default in recent emacsen
 
 ;; packages
-;; I added this for as far as I can tell no fucking reason. leaving it because
-;; i am afraid
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  )
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+
+;; load custom themes
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/org/personal.org"))))
+ '(custom-enabled-themes (quote (flatui)))
+ '(custom-safe-themes
+   (quote
+    ("15348febfa2266c4def59a08ef2846f6032c0797f001d7b9148f30ace0d08bcf" default)))
+ '(package-selected-packages (quote (flatui-theme powershell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;; load custom theme
-(add-hook 'after-init-hook 
-	  (lambda () (load-theme 'cyberpunk t)))
