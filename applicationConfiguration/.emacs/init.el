5a470007b3822a4e02fbdb782fed09c4be9bfac0
jowj <me@jowj.net>
2019-02-20 23:06:23 -0600

    Enable manoj-dark theme.

--- applicationConfiguration/.emacs/init.el
+++ applicationConfiguration/.emacs/init.el
@@ -144,6 +144,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(manoj-dark))
  '(custom-safe-themes
    '("356e5cbe0874b444263f3e1f9fffd4ae4c82c1b07fe085ba26e2a6d332db34dd" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(org-agenda-files
