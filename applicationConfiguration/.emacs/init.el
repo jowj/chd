feffbf76507d604a90607389d12090cbc5d81985
jowj <me@jowj.net>
2019-05-31 19:03:49 -0500

    Enable emojify mode.

--- applicationConfiguration/.emacs/init.el
+++ applicationConfiguration/.emacs/init.el
@@ -21,6 +21,7 @@
     racer
     helm
     eyebrowse
+    emojify
     ;;company-mode
     org2blog
     pylint
@@ -73,14 +74,14 @@ jlj-Packages)
     (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
  '(package-selected-packages
    (quote
-    (ansible yaml-mode smex twittering-mode znc eyebrowse helm racer flycheck-rust rust-mode color-theme-sanityinc-tomorrow org2blog multiple-cursors flymake-python-pyflakes pdf-tools weechat jedi python-mode pylint py-autopep8 powershell outline-magic markdown-mode magit flycheck exec-path-from-shell elpygen elpy ein doom-themes csharp-mode)))
+    (emojify dockerfile-mode docker yaml-mode ansible smex twittering-mode znc eyebrowse helm racer flycheck-rust rust-mode color-theme-sanityinc-tomorrow org2blog multiple-cursors flymake-python-pyflakes pdf-tools weechat jedi python-mode pylint py-autopep8 powershell outline-magic markdown-mode magit flycheck exec-path-from-shell elpygen elpy ein doom-themes csharp-mode)))
  '(znc-servers
    (\`
     (("bouncer.awful.club" 5000 t
       ((freenode "blindidiotgod/freenode"
-		 (\, znc-password))
+                 (\, znc-password))
        (OFTC "blindidiotgod/OFTC"
-	     (\, znc-password))))))))
+             (\, znc-password))))))))
 
 ;;;; run emacs as server (connect to it with `emacsclient`)
 (server-start)
