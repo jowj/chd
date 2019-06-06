1da9e08fc1dca43dc2c309f394ecbc8d0540a849
jowj <me@jowj.net>
2019-06-06 13:28:56 -0500

    Move to mono file to prepare for org-mode init file.

--- applicationConfiguration/.emacs/jlj-helm.el
+++
@@ -1,6 +0,0 @@
-(defun helm-surfraw-duck (x)
-  "Search duckduckgo in default browser"
-  (interactive "sSEARCH:")
-  (helm-surfraw x "duckduckgo" ))
-(global-set-key (kbd "C-c s") 'helm-surfraw-duck)
-
