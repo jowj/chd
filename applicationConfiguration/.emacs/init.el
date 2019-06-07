f3bf76de8b6fb0bb3d68a661b4665eb9fbbf7d97
jowj <me@jowj.net>
2019-06-07 10:40:04 -0500

    Move custom-set vars to the proper init.el file.

--- applicationConfiguration/.emacs/init.el
+++ applicationConfiguration/.emacs/init.el
@@ -20,3 +20,25 @@
 
 
 (org-babel-load-file (expand-file-name "~/Documents/projects/agares/applicationConfiguration/.emacs/jlj-init.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(epg-gpg-program "/usr/local/bin/gpg")
+ '(package-selected-packages
+   (quote
+    (ox-reveal org-reveal jedi znc yaml-mode which-key use-package twittering-mode try smex racer python-mode pylint py-autopep8 powershell pdf-tools outline-magic org2blog multiple-cursors magit helm flycheck-rust eyebrowse exec-path-from-shell emojify elpy ein doom-themes dockerfile-mode docker csharp-mode ansible anaconda-mode ace-window)))
+ '(znc-servers
+   (\`
+    (("bouncer.awful.club" 5000 t
+      ((freenode "blindidiotgod/freenode"
+		 (\, znc-password))
+       (OFTC "blindidiotgod/OFTC"
+	     (\, znc-password))))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
