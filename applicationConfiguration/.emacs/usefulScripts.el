b44ac1f638728656d846bf197f58b36ce45f5951
jowj <me@jowj.net>
2019-06-10 13:34:13 -0500

    Update with interesting org-mode snippet.

--- applicationConfiguration/.emacs/usefulScripts.el
+++ applicationConfiguration/.emacs/usefulScripts.el
@@ -1,3 +1,5 @@
+;; org-mode shit.
+
 (defun my/org-mode-hook ()
   "Stop the org-level headers from increasing in height relative to the other text."
   (dolist (face '(org-level-1
@@ -7,3 +9,6 @@
                   org-level-5))
     (set-face-attribute face nil :weight 'semi-bold :height 1.0)))
 (add-hook 'org-mode-hook 'my/org-mode-hook)
+
+;; makes a task show up m-f, but you can't cycle todos;
+<%%(memq (calendar-day-of-week date) '(1 2 3 4 5))>
