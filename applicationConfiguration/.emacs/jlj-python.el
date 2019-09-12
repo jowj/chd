79c9f59adafb12de592cfd9c8fee2f65b76e4aa1
josiah <josiah@slowling.local>
2019-09-12 23:52:57 +0000

    Add entry about eglot to help me to remember to add more

--- applicationConfiguration/.emacs/jlj-python.el
+++ applicationConfiguration/.emacs/jlj-python.el
@@ -4,27 +4,30 @@
 ;; using jedi requires virtualenv to be installed
 ;; pipenv is mostly acceptable but i could not make a full IDE experience happen
 
-(use-package pylint
-  :ensure t)
+;; (use-package pylint
+;;   :ensure t)
 
 
-(setq python-shell-interpreter "/usr/local/bin/python3"
-      python-shell-interpreter-args "-i")
+;; (setq python-shell-interpreter "/usr/local/bin/python3"
+;;       python-shell-interpreter-args "-i")
 
-(use-package py-autopep8
-  :ensure t
-  :config
-  (progn
-    (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)))
+;; (use-package py-autopep8
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)))
 
-(use-package jedi
-  :ensure t
-  :init
-  (add-hook 'python-mode-hook 'jedi:setup)
-  (add-hook 'python-mode-hook 'jedi:ac-setup))
+;; (use-package jedi
+;;   :ensure t
+;;   :init
+;;   (add-hook 'python-mode-hook 'jedi:setup)
+;;   (add-hook 'python-mode-hook 'jedi:ac-setup))
 
 (use-package pipenv
   :ensure t)
 
-(use-package pyvenv
+(use-package eglot
   :ensure t)
+
+;; (use-package pyvenv
+;;   :ensure t)
