95a0ad5d94e8cbb779e721a12371199692729af4
jowj <me@jowj.net>
2020-03-01 15:29:16 -0600

    Switch to lsp for golang.

--- .emacs.d/jlj-golang.el
+++ .emacs.d/jlj-golang.el
@@ -1,6 +1,6 @@
-(use-package eglot
+(use-package lsp-mode
   :ensure t
   :config
-  (add-hook 'go-mode-hook 'eglot-ensure)
+  (add-hook 'go-mode-hook 'lsp-ensure)
   (local-set-key "\C-x\C-m" 'compile)
   (setq compile-command "go test -v && go vet && golint"))
