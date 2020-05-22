9a71d847d6c9433c4abd4391e74969c398bc5101
josiah <me@jowj.net>
2020-05-22 18:40:39 -0500

    This is the proper color to match my rofi skin.

--- .config/awesome/jlj-theme.lua
+++ .config/awesome/jlj-theme.lua
@@ -13,7 +13,7 @@ local theme = {}
 
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
+theme.bg_normal     = "#2f1e2e"
 theme.bg_focus      = "#535d6c"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
