12f8744211a5ab1c2c2d7c0a7481cccf21492f94
josiah <me@jowj.net>
2020-05-22 18:41:49 -0500

    Proper select color.
    - I've made something vile lmao, i'm kepeing this.

--- .config/awesome/jlj-theme.lua
+++ .config/awesome/jlj-theme.lua
@@ -14,7 +14,7 @@ local theme = {}
 theme.font          = "sans 8"
 
 theme.bg_normal     = "#2f1e2e"
-theme.bg_focus      = "#535d6c"
+theme.bg_focus      = "#815ba4"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
