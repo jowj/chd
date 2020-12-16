e351e708b6927c4bb5062d6c2acda9818abe7664
jowj <me@jowj.net>
2020-12-16 13:52:03 -0600

    Move to reasonable pyls setup

--- .emacs.d/jlj-python.el
+++ .emacs.d/jlj-python.el
@@ -37,6 +37,13 @@
   :ensure t
   :hook (lsp-mode . lsp-ui-mode))
 
+(use-package lsp-python-ms
+  :ensure t
+  :init (setq lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-python-ms)
+                          (lsp))))  ; or lsp-deferred
+
 
 ;; (use-package pyvenv
 ;;   :ensure t)
