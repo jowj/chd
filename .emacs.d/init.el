95752820cdd21bffab8d4caa7348f9aebbfe14b6
Josiah Ledbetter <josiah.ledbetter@josiah.ledbetter-AJJM>
2021-03-08 11:37:28 -0600

    Move stuff over to a custom.el file.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,6 +17,10 @@
 (eval-when-compile
   (require 'use-package))
 
+;; custom shit should go somewhere else:
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;; load my files
 (load-file "~/.emacs.d/org-protocol-capture-html.el")
 (load-file "~/.emacs.d/src/keychain-environment.el")
@@ -31,24 +35,3 @@
 (when (eq system-type 'gnu/linux)
   (keychain-refresh-environment)
   )
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(epg-gpg-program "/usr/local/bin/gpg")
- '(global-hl-line-mode t)
- '(package-selected-packages
-   '(sudo-edit modus-themes org-caldav json-mode pdf-tools org-special-block-extras neotree sr-speedbar lsp-python-ms fira-code-mode yasnippet org-pdftools phps-mode projectile webfeeder znc pinboard yaml-mode which-key web-mode virtualenvwrapper use-package twittering-mode try smex racer powershell poetry pipenv ox-reveal outline-magic org-pdfview org-bullets ob-restclient multiple-cursors magit lua-mode lsp-ui iedit helm flycheck-rust exec-path-from-shell eglot edit-indirect company-lsp ansible ace-window)))
- '(znc-servers
-   `(("bouncer.awful.club" 5000 t
-      ((freenode "blindidiotgod/freenode" ,znc-password)
-       (OFTC "blindidiotgod/OFTC" ,znc-password)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
