140e21dd566c86910d418804868ae1b79bd1bdda
Josiah Ledbetter <josiah.ledbetter@unity3d.com>
2023-07-20 11:53:43 -0500

    Add vs:code shortcut.

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -92,7 +92,8 @@ appCuts = {
   o = 'Calendar.app',
   p = 'Bitwarden',
   t = 'iTerm.app',
-  z = 'Zoom.us'
+  z = 'Zoom.us',
+  v = 'Visual Studio Code'  
 }
 
 modalHotKey = dofile(os.getenv("HOME") .. "/.config/hammerspoon/modalHotKey.lua")
