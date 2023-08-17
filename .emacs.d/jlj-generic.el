b0e7a36e999e91dd07011d7a022aae6dd0171b57
josiah <me@jowj.net>
2023-08-16 19:53:47 -0500

    Setup terraform mode.

--- .emacs.d/jlj-generic.el
+++ .emacs.d/jlj-generic.el
@@ -218,6 +218,17 @@
 (use-package ansible
   :ensure t)
 
+(use-package terraform-mode
+  :ensure t
+  :custom (terraform-indent-level 4)
+  :config
+  (defun my-terraform-mode-init ()
+    ;; if you want to use outline-minor-mode
+    ;; (outline-minor-mode 1)
+    )
+
+  (add-hook 'terraform-mode-hook 'my-terraform-mode-init))
+
 (use-package yaml-mode
   :ensure t)
 
